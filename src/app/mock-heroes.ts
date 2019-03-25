3dbac0c5f827318b23d1641e63970ff12dbad83e
Nguyen Ngoc Duy <nguyenngoc.duy20@gmail.com>
2019-03-25 16:08:51 +0700

    update 16:08

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,7 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Tuan Tran', skill: 'Web Fronend', description: '' },
+    { id: 12, name: 'Trai Nguyen', skill: 'Microsoft Office 365', description: '' },
+    { id: 13, name: 'Bao Huynh', skill: 'Microsoft Azure', description: ''}
+];
