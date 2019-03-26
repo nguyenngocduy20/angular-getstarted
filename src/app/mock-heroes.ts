d2297bf27f91382d43bd687b83030318eb90ff4e
Nguyen Ngoc Duy <nguyenngoc.duy20@gmail.com>
2019-03-26 11:45:53 +0700

    angular service update

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -3,5 +3,6 @@ import { Hero } from './hero';
 export const HEROES: Hero[] = [
     { id: 11, name: 'Tuan Tran', skill: 'Web Fronend', description: '' },
     { id: 12, name: 'Trai Nguyen', skill: 'Microsoft Office 365', description: '' },
-    { id: 13, name: 'Bao Huynh', skill: 'Microsoft Azure', description: ''}
+    { id: 13, name: 'Bao Huynh', skill: 'Microsoft Azure', description: ''},
+    { id: 14, name: 'Ho Nguyen', skill: 'Microsoft Office 365', description: ''}
 ];
